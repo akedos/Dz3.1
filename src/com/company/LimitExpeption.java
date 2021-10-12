df887bd6148fc563f21a8609b51fa5839e798f99
Akedos <mukashevakedos02@gmail.com>
2021-10-12 17:37:18 +0600

    Initial commit

---
+++ src/com/company/LimitExpeption.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class LimitException extends Exception{
+    public LimitException(String message, double remainingAmount) {
+        super(message);
+    }
+
+    public static double getRemainingAmount() {return 0;}
+}
